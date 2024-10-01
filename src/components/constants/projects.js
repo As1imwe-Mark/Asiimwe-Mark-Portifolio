4d2de012746c75a9bfd61c4e160b37e938fba124
Asiimwe Mark <markasiimwe75@gmail.com>
2024-10-01 09:50:23 +0300

    Add new projects

--- src/components/constants/projects.js
+++ src/components/constants/projects.js
@@ -5,10 +5,16 @@ import math from '../../assets/math Magician.png';
 import Faqs from '../../assets/Faqs.png';
 import awesome from '../../assets/Awesome books.png';
 import webcamp from '../../assets/webcapstone.png';
+import graphics from '../../assets/graphics.png'
+import amazon from '../../assets/amazon.png'
+import movie from '../../assets/movie.png'
 
 export const projects = [
-  { img: linkwarden, title: "LinkWarden", desc: "It is a project I designed for practice from a figma design sent to me by my friend on X. I used React and TailwindCss", github:"https://github.com/As1imwe-Mark/test-app",live:"https://linkwardenapp.netlify.app/" },
+  { img: linkwarden, title: "LinkWarden", desc: "It is a desktop project I designed for practice from a figma design sent to me by my friend on X. I used React and TailwindCss", github:"https://github.com/As1imwe-Mark/test-app",live:"https://linkwardenapp.netlify.app/" },
   { img: livingSeed , title: "Living Seed Academy", desc: "It is a website I designed for Living Seed Academy School, I used React, TailwindCss and Sanity as backend", github:"https://github.com/As1imwe-Mark/Living_Seed-Academy",live:"https://livingseedacademy.netlify.app" },
+  { img: graphics, title: "Graphics Landing Page", desc: "It is a desktop project I designed for practice from a figma design sent to me by my friend on X. I used React and TailwindCss", github:"https://github.com/As1imwe-Mark/Graphics-Ltd",live:"https://graphicsltd.netlify.app/"  },
+  { img:amazon, title: "Amazon Clone", desc: "It is a project I designed for practice while learning fundamentals of javaScript, I used HTML, CSS and JavaScript", github:"https://github.com/As1imwe-Mark/Amazon-clone",live:"https://amazonclonee75.netlify.app/"  },
+  { img: movie, title: "Movie Board", desc: "The Movieboard project is an App that allows users to keep track of their favorite movies, shows, and series. The user can add a like and a comment to their favorite show. The project makes an API call to TVMave API and Microverse's Involvement API to update the UI and keep track of the comments and likes.,  I used HTML, CSS and JavaScript", github:"https://github.com/As1imwe-Mark/js-group-capstone.",live:"https://movieboard75.netlify.app/"  },
   { img: Faqs, title: "FAQs", desc: "It is a project I designed as a challenge from Frontend Mentor, I used React and TailwindCss", github:"https://github.com/As1imwe-Mark/FAQs",live:"https://markfaq.netlify.app/"  },
   { img: todoapp, title: "TodoList App", desc: "It is a project I designed as a challenge from Frontend Mentor, I used React and TailwindCss", github:"https://github.com/As1imwe-Mark/TodoApp",live:"https://marktodoapp.netlify.app/"  },
   { img: math, title: "Project #5", desc: "This is a Microverse project under the react module 3 block 1. It is a single page Application that contains a calculator that can do simple Math operations plus a random Math quotations from an external API.", github:"https://github.com/As1imwe-Mark/math_magician",live:"https://mathmagician75.netlify.app/"  },
